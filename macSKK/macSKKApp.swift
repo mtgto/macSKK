72ec9df1b59d450139c7750d2b27eb550eb0c3e7
mtgto <hogerappa@gmail.com>
2022-12-24 18:54:47 +0900

    Initial commit

---
+++ macSKK/macSKKApp.swift
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: 2022 mtgto <hogerappa@gmail.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+import SwiftUI
+
+@main
+struct macSKKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
