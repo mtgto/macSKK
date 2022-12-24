fffb56be658b26e00b5fa4044c6ad493edf0e242
mtgto <hogerappa@gmail.com>
2022-12-24 19:32:36 +0900

    Add InputController

---
+++ macSKK/InputController.swift
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2022 mtgto <hogerappa@gmail.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+import Foundation
+import InputMethodKit
+
+@objc(InputController)
+class InputController: IMKInputController {
+    
+}
--- macSKK/macSKKApp.swift
+++ macSKK/macSKKApp.swift
@@ -1,10 +1,25 @@
 // SPDX-FileCopyrightText: 2022 mtgto <hogerappa@gmail.com>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+import InputMethodKit
 import SwiftUI
+import os
+
+let logger: Logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "main")
 
 @main
 struct macSKKApp: App {
+    private var server: IMKServer!
+
+    init() {
+        if !self.isTest() {
+            guard let connectionName = Bundle.main.infoDictionary?["InputMethodConnectionName"] as? String else {
+                fatalError("InputMethodConnectionName is not set")
+            }
+            self.server = IMKServer(name: connectionName, bundleIdentifier: Bundle.main.bundleIdentifier)
+        }
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
