78be7f4ad412c0507943b0a102eef8cb45f07df5
mtgto <hogerappa@gmail.com>
2023-08-09 23:17:07 +0900

    リリースXMLのパース処理をリファクタ

--- macSKK/UpdateChecker.swift
+++ macSKK/UpdateChecker.swift
@@ -25,7 +25,8 @@ struct UpdateChecker {
         let response = try await proxy.fetch()
         return String(data: response, encoding: .utf8) ?? "ERROR2"
     }
-    
+
+    // atomをパースして返すので新しい順で返す
     func fetch() async throws -> [Release] {
         let service = NSXPCConnection(serviceName: "net.mtgto.inputmethod.macSKK.FetchUpdateService")
         service.remoteObjectInterface = NSXPCInterface(with: FetchUpdateServiceProtocol.self)
@@ -40,25 +41,28 @@ struct UpdateChecker {
         }
         let response = try await proxy.fetch()
         let doc = try XMLDocument(data: response)
-        let releases = try doc.nodes(forXPath: "feed/entry").compactMap { node -> Release? in
+        return try parseDocument(doc)
+    }
+
+    func parseDocument(_ doc: XMLDocument) throws -> [Release] {
+        return try doc.nodes(forXPath: "feed/entry").compactMap { node -> Release? in
             try parseEntryNode(node)
         }
-        return releases
     }
-    
+
     func parseEntryNode(_ node: XMLNode) throws -> Release? {
         guard let version = try node.nodes(forXPath: "title").first?.stringValue else {
             logger.warning("リリース情報のXMLが不正です (title)")
             return nil
         }
-        guard let link = try node.nodes(forXPath: "link/@href").first?.stringValue, let linkUrl = URL(string: link) else {
+        guard let link = try node.nodes(forXPath: "link/@href").first?.stringValue.flatMap({ URL(string: $0) }) else {
             logger.warning("リリース情報のXMLが不正です (link)")
             return nil
         }
-        guard let updatedStr = try node.nodes(forXPath: "updated").first?.stringValue, let updated = ISO8601DateFormatter().date(from: updatedStr) else {
+        guard let updated = try node.nodes(forXPath: "updated").first?.stringValue.flatMap({ ISO8601DateFormatter().date(from: $0) }) else {
             logger.warning("リリース情報のXMLが不正です (updated)")
             return nil
         }
-        return Release(version: version, updated: updated, url: linkUrl)
+        return Release(version: version, updated: updated, url: link)
     }
 }
--- macSKKTests/UpdateCheckerTests.swift
+++ macSKKTests/UpdateCheckerTests.swift
@@ -6,15 +6,15 @@ import XCTest
 @testable import macSKK
 
 final class UpdateCheckerTests: XCTestCase {
-    func testParseEntryNode() throws {
+    func testParseDocument() throws {
         let updateChecker = UpdateChecker()
         let xml = Bundle(for: Self.self).url(forResource: "releases", withExtension: "atom")!
         let doc = try XMLDocument(contentsOf: xml)
-        let releaseNodes = try doc.nodes(forXPath: "feed/entry")
-        let releases = try releaseNodes.map { try updateChecker.parseEntryNode($0) }
-        XCTAssertEqual(releases.first??.version, "0.1.1")
-        XCTAssertEqual(releases.first??.url.absoluteString, "https://github.com/mtgto/macSKK/releases/tag/0.1.1")
-        XCTAssertEqual(releases.first??.updated, ISO8601DateFormatter().date(from: "2023-08-09T09:21:49Z"))
-        XCTAssertEqual(releases[1]?.version, "0.1.0")
+        let releases = try updateChecker.parseDocument(doc)
+        XCTAssertEqual(releases.count, 2)
+        XCTAssertEqual(releases.first?.version, "0.1.1")
+        XCTAssertEqual(releases.first?.url.absoluteString, "https://github.com/mtgto/macSKK/releases/tag/0.1.1")
+        XCTAssertEqual(releases.first?.updated, ISO8601DateFormatter().date(from: "2023-08-09T09:21:49Z"))
+        XCTAssertEqual(releases[1].version, "0.1.0")
     }
 }
