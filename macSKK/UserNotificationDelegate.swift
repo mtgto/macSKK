76e24b3370e169a2a75bd9c5157109800b18192c
mtgto <hogerappa@gmail.com>
2023-08-19 11:53:34 +0900

    アプリアイコンを暫定で追加

--- macSKK/UserNotificationDelegate.swift
+++ macSKK/UserNotificationDelegate.swift
@@ -6,13 +6,14 @@ import UserNotifications
 
 class UserNotificationDelegate: NSObject, UNUserNotificationCenterDelegate {
     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-        logger.log("UserNotification didReceive \(response.notification.request.identifier, privacy: .public)")
         if response.notification.request.identifier == Release.userNotificationIdentifier {
             if let userInfo = response.notification.request.content.userInfo[Release.userNotificationUserInfoKey] as? [String: Any],
                let urlString = userInfo[Release.userNotificationUserInfoNameUrl] as? String,
                let url = URL(string: urlString) {
                 // リリースページを開く
-                _ = NSWorkspace.shared.open(url)
+                if !NSWorkspace.shared.open(url) {
+                    logger.warning("新しいバージョンの通知をタップしたがリリースページを開くことができませんでした。")
+                }
             }
         }
     }
