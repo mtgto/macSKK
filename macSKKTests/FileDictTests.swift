8452b5dc51023f29e95006833c2192226f34d65f
mtgto <hogerappa@gmail.com>
2023-09-18 12:34:41 +0900

    辞書のシリアライズのテストに更新・削除のパターンを追加

--- macSKKTests/FileDictTests.swift
+++ macSKKTests/FileDictTests.swift
@@ -35,12 +35,40 @@ final class FileDictTests: XCTestCase {
         dict.add(yomi: "あ", word: Word("阿", annotation: Annotation(dictId: "testDict", text: "阿の注釈")))
         dict.add(yomi: "あr", word: Word("有", annotation: Annotation(dictId: "testDict", text: "有の注釈")))
         dict.add(yomi: "あr", word: Word("在", annotation: Annotation(dictId: "testDict", text: "在の注釈")))
-        let expected = [
+        var expected = [
             FileDict.headers[0],
             FileDict.okuriAriHeader,
             "あr /在;在の注釈/有;有の注釈/",
             FileDict.okuriNashiHeader,
             "あ /阿;阿の注釈/亜;亜の注釈/",
+            "",
+        ].joined(separator: "\n")
+        XCTAssertEqual(dict.serialize(), expected)
+        // 追加したエントリはシリアライズ時は先頭に付く
+        dict.add(yomi: "い", word: Word("伊"))
+        dict.add(yomi: "いr", word: Word("射"))
+        expected = [
+            FileDict.headers[0],
+            FileDict.okuriAriHeader,
+            "いr /射/",
+            "あr /在;在の注釈/有;有の注釈/",
+            FileDict.okuriNashiHeader,
+            "い /伊/",
+            "あ /阿;阿の注釈/亜;亜の注釈/",
+            "",
+        ].joined(separator: "\n")
+        XCTAssertEqual(dict.serialize(), expected)
+        // 追加更新した場合は順序を変更する。削除更新した場合は順序を変更しない
+        XCTAssertTrue(dict.delete(yomi: "あ", word: "亜"))
+        dict.add(yomi: "あr", word: Word("或"))
+        expected = [
+            FileDict.headers[0],
+            FileDict.okuriAriHeader,
+            "あr /或/在;在の注釈/有;有の注釈/",
+            "いr /射/",
+            FileDict.okuriNashiHeader,
+            "い /伊/",
+            "あ /阿;阿の注釈/",
             "",
         ].joined(separator: "\n")
         XCTAssertEqual(dict.serialize(), expected)
