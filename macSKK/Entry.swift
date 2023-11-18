0b7e3df71e586e67368fffb8bd199037dbc8a265
mtgto <hogerappa@gmail.com>
2023-11-18 16:00:21 +0900

    SKK-JISYO.Lの「こう」のように変換候補が多いとスタックオーバーフローするのでループ処理に書き直し

--- macSKK/Entry.swift
+++ macSKK/Entry.swift
@@ -70,6 +70,48 @@ struct Entry {
      *   - wordsText "胃/異/" のように先頭のスラッシュを除いた変換候補部分の文字列
      */
     static func parseWords(_ wordsText: Substring, dictId: FileDict.ID) -> [Word]? {
+        // 長すぎるとスタックオーバーフローで死ぬので送り仮名ブロック以外はループで処理
+        var wordsText = wordsText
+        var result: [Word] = []
+        while true {
+            if wordsText.isEmpty {
+                return result
+            } else if wordsText.first == "/" || wordsText.last != "/" {
+                return nil
+            }
+            if wordsText.first == "[" {
+                // 送り仮名ブロックのパース ややこしいからリファクタしたい
+                let array = wordsText.dropFirst().split(separator: "]", maxSplits: 1)
+                if array.count != 2 || array[1].first != "/" {
+                    return nil
+                }
+                guard let index = array[0].firstIndex(of: "/") else { return nil }
+                if index == array[0].startIndex || index == array[0].endIndex {
+                    return nil
+                }
+                let yomi = array[0].prefix(upTo: index)
+                let words = parseWords(array[0].suffix(from: index).dropFirst(), dictId: dictId)?.map { word in
+                    Word(word.word, okuri: String(yomi), annotation: word.annotation)
+                }
+                guard let words else { return nil }
+                result.append(contentsOf: words)
+                wordsText = array[1].dropFirst()
+            } else {
+                let array = wordsText.split(separator: "/", maxSplits: 1)
+                switch array.count {
+                case 1:
+                    result.append(parseWord(array[0], dictId: dictId))
+                    return result
+                case 2:
+                    result.append(parseWord(array[0], dictId: dictId))
+                    wordsText = array[1]
+                default:
+                    return nil
+                }
+            }
+        }
+
+
         if wordsText.isEmpty {
             return []
         } else if wordsText.first == "/" || wordsText.last != "/" {
