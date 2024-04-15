fb677af0072804267ad02d85a9b875ac580b2fc0
YOSHIMURA Yuu <yyu@mental.poker>
2024-04-15 13:38:06 +0900

    Update build_restart.sh
    Co-authored-by: Satoshi Goto <hogerappa@gmail.com>

--- build_restart.sh
+++ build_restart.sh
@@ -4,7 +4,7 @@ set -e
 # ビルド
 xcodebuild -workspace macSKK.xcodeproj/project.xcworkspace -scheme macSKK clean archive -archivePath build/archive.xcarchive
 # 上書き
-sudo rm -rf /Library/Input\ Methods/macSKK.app
+rm -rf ~/Library/Input\ Methods/macSKK.app
 sudo cp -r build/archive.xcarchive/Products/Library/Input\ Methods/macSKK.app ~/Library/Input\ Methods/
 # 再起動
 pkill "macSKK"
