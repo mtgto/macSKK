ae2389c25ac17f7be3c2aa293a6fc1dc73a2f379
mtgto <hogerappa@gmail.com>
2024-05-03 12:53:34 +0900

    NSEvent.ModifierFlagsをそのまま使うようにする

--- macSKK/KeyBinding.swift
+++ macSKK/KeyBinding.swift
@@ -24,43 +24,60 @@ struct KeyBinding {
         case stickyShift
     }
 
-    struct ModifierFlags: OptionSet {
-        let rawValue: Int
-
-        static let control = Self(rawValue: 1 << 0)
-        static let shift = Self(rawValue: 1 << 1)
-    }
-
-    struct Value {
+    struct Value: Hashable {
         let keyCode: UInt16
-        let modifiers: ModifierFlags
+        let modifierFlags: NSEvent.ModifierFlags
+
+        func hash(into hasher: inout Hasher) {
+            hasher.combine(keyCode)
+            hasher.combine(modifierFlags.rawValue)
+        }
+
+        init(event: NSEvent) {
+            keyCode = event.keyCode
+            modifierFlags = event.modifierFlags
+        }
+
+        init(keyCode: UInt16, modifierFlags: NSEvent.ModifierFlags) {
+            self.keyCode = keyCode
+            self.modifierFlags = modifierFlags
+        }
     }
 
-    let values: [Key: [Value]] = [:]
+    let values: [Key: [Value]]
+    let dict: [Value: Key]
 
     /// デフォルトのキーバインディング
-    static var defaultKeyBinding: [Key: [Value]] {
+    static var defaultKeyBindingSettings: [Key: [Value]] {
         return Dictionary(uniqueKeysWithValues: Key.allCases.map { key in
             switch key {
             case .toggleHiragana:
-                return (key, [Value(keyCode: 0x26, modifiers: .control)])
+                return (key, [Value(keyCode: 0x26, modifierFlags: .control)])
             case .toggleKana:
-                return (key, [Value(keyCode: 0x0c, modifiers: [])])
+                return (key, [Value(keyCode: 0x0c, modifierFlags: [])])
             case .toggleHankakuKana:
-                return (key, [Value(keyCode: 0x0c, modifiers: .control)])
+                return (key, [Value(keyCode: 0x0c, modifierFlags: .control)])
             case .direct:
-                return (key, [Value(keyCode: 0x25, modifiers: [])])
+                return (key, [Value(keyCode: 0x25, modifierFlags: [])])
             case .zenkaku:
-                return (key, [Value(keyCode: 0x25, modifiers: .shift)])
+                return (key, [Value(keyCode: 0x25, modifierFlags: .shift)])
             case .abbrev:
-                return (key, [Value(keyCode: 0x2c, modifiers: [])])
+                return (key, [Value(keyCode: 0x2c, modifierFlags: [])])
             case .stickyShift:
-                return (key, [Value(keyCode: 0x29, modifiers: [])])
+                return (key, [Value(keyCode: 0x29, modifierFlags: [])])
             }
+        })
+    }
+    static let defaultKeyBinding = KeyBinding(KeyBinding.defaultKeyBindingSettings)
+
+    init(_ values: [Key: [Value]]) {
+        self.values = values
+        self.dict = Dictionary(uniqueKeysWithValues: values.flatMap { keyValue in
+            keyValue.value.map { ($0, keyValue.key) }
         })
     }
 
     func key(event: NSEvent) -> Key? {
-        return nil
+        return dict[Value(event: event)]
     }
 }
