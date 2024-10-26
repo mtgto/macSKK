71e9b9386c10c06c85b65594dfb1c49eedaa386a
mtgto <hogerappa@gmail.com>
2024-10-26 21:18:12 +0900

    コピペによるデッドコードを削除

--- macSKK/Entry.swift
+++ macSKK/Entry.swift
@@ -136,43 +136,6 @@ struct Entry: Sendable {
                 }
             }
         }
-
-
-        if wordsText.isEmpty {
-            return []
-        } else if wordsText.first == "/" || wordsText.last != "/" {
-            return nil
-        }
-        if wordsText.first == "[" {
-            // 送り仮名ブロックのパース ややこしいからリファクタしたい
-            let array = wordsText.dropFirst().split(separator: "]", maxSplits: 1)
-            if array.count != 2 || array[1].first != "/" {
-                return nil
-            }
-            guard let index = array[0].firstIndex(of: "/") else { return nil }
-            if index == array[0].startIndex || index == array[0].endIndex {
-                return nil
-            }
-            let yomi = array[0].prefix(upTo: index)
-            let words = parseWords(array[0].suffix(from: index).dropFirst(), dictId: dictId)?.map { word in
-                Word(word.word, okuri: String(yomi), annotation: word.annotation)
-            }
-            guard let words else { return nil }
-            guard let rest = parseWords(array[1].dropFirst(), dictId: dictId) else { return nil }
-            return words + rest
-        } else {
-            let array = wordsText.split(separator: "/", maxSplits: 1)
-            switch array.count {
-            case 1:
-                return [parseWord(array[0], dictId: dictId)]
-            case 2:
-                let word = parseWord(array[0], dictId: dictId)
-                guard let words = parseWords(array[1], dictId: dictId) else { return nil }
-                return [word] + words
-            default:
-                return nil
-            }
-        }
     }
 
     static func parseWord(_ wordText: Substring, dictId: FileDict.ID) -> Word {
