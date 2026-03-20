6cedb10c8a5ef63bdfa0d899e202b266a688a968
mtgto <hogerappa@gmail.com>
2026-03-20 15:45:27 +0900

    handleInputの無意味なwhile trueループを削除

--- SKKServClient/Network/SKKServProtocol.swift
+++ SKKServClient/Network/SKKServProtocol.swift
@@ -16,26 +16,23 @@ final class SKKServProtocol: NWProtocolFramerImplementation {
     }
 
     func handleInput(framer: NWProtocolFramer.Instance) -> Int {
-        while true {
-            var received: Data? = nil
-            _ = framer.parseInput(minimumIncompleteLength: 1, maximumLength: 1024 * 1024) { buffer, isComplete -> Int in
-                // 直前のリクエストがサーバーのバージョン要求、サーバーのホスト名とIPアドレスのリスト要求の場合はスペースが終端記号となりLFは送られない
-                // NOTE: 2024-03-15現在、yaskkserv2はIPアドレスのリスト要求の場合、スペースが終端記号になっていない
-                if let lastRequest, let buffer, let index = buffer.firstIndex(of: lastRequest.terminateCharacter) {
-                    buffer[0..<index].withUnsafeBytes { pointer in
-                        received = Data(pointer)
-                    }
+        var received: Data? = nil
+        _ = framer.parseInput(minimumIncompleteLength: 1, maximumLength: 1024 * 1024) { buffer, isComplete -> Int in
+            // 直前のリクエストがサーバーのバージョン要求、サーバーのホスト名とIPアドレスのリスト要求の場合はスペースが終端記号となりLFは送られない
+            // NOTE: 2024-03-15現在、yaskkserv2はIPアドレスのリスト要求の場合、スペースが終端記号になっていない
+            if let lastRequest, let buffer, let index = buffer.firstIndex(of: lastRequest.terminateCharacter) {
+                buffer[0..<index].withUnsafeBytes { pointer in
+                    received = Data(pointer)
                 }
-                return 0
             }
-            guard let received else {
-                return 0
-            }
-            let message = NWProtocolFramer.Message(response: received)
-            _ = framer.deliverInputNoCopy(length: received.count + 1, message: message, isComplete: true)
-            // 次のメッセージを待つ
             return 0
         }
+        guard let received else {
+            return 0
+        }
+        let message = NWProtocolFramer.Message(response: received)
+        _ = framer.deliverInputNoCopy(length: received.count + 1, message: message, isComplete: true)
+        return 0
     }
 
     func handleOutput(framer: NWProtocolFramer.Instance, message: NWProtocolFramer.Message, messageLength: Int, isComplete: Bool) {
